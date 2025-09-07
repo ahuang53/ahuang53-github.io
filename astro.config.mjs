9d48ac5bb3f5f1af7e1918ce941567bbdb71a094
AndyH <andyhu35@gmail.com>
2025-09-07 01:50:36 -0400

    github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ahuangdac.com",
   vite: {
     plugins: [tailwindcss()],
   },
